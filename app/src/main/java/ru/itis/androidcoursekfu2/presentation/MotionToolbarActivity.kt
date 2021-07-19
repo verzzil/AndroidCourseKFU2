4fb61011701cff29854d3d45e981b8219e251fc3
Albert Khannanov <xannanov.albert@mail.ru>
2021-07-19 14:14:30 +0300

    dropdownadapter

--- app/src/main/java/ru/itis/androidcoursekfu2/presentation/MotionToolbarActivity.kt
+++ app/src/main/java/ru/itis/androidcoursekfu2/presentation/MotionToolbarActivity.kt
@@ -2,16 +2,25 @@ package ru.itis.androidcoursekfu2.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.widget.Guideline
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.*
 import ru.itis.androidcoursekfu2.R
+import ru.itis.androidcoursekfu2.presentation.adapter.DropDownAdapter
+import ru.itis.androidcoursekfu2.presentation.models.TestModel
 import ru.itis.androidcoursekfu2.utils.awaitTransitionComplete
 
 class MotionToolbarActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
     private lateinit var motion: MotionLayout
     private lateinit var img: ImageView
+    private lateinit var guideline: Guideline
+    private lateinit var rv: RecyclerView
+
+    private lateinit var adapter: DropDownAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +28,14 @@ class MotionToolbarActivity : AppCompatActivity(), CoroutineScope by MainScope()
 
         motion = findViewById(R.id.motion)
         img = findViewById(R.id.img_view)
+        guideline = findViewById(R.id.guideline1)
+        rv = findViewById(R.id.rv)
+
+        adapter = DropDownAdapter(TestModel.getBigData()) {
+            Log.i("clicked","Ω")
+        }
+
+        rv.adapter = adapter
 
         img.setOnClickListener {
             launch {
@@ -32,10 +49,14 @@ class MotionToolbarActivity : AppCompatActivity(), CoroutineScope by MainScope()
 
                 motion.setTransition(R.id.third_set, R.id.fourth_set)
                 motion.transitionToEnd()
+                motion.awaitTransitionComplete(R.id.fourth_set)
+
+                motion.setTransition(R.id.fourth_set, R.id.fifth_set)
+                motion.transitionToEnd()
             }
-
-
         }
+
+
 
     }
 
--- app/src/main/java/ru/itis/androidcoursekfu2/presentation/models/TestModel.kt
+++ app/src/main/java/ru/itis/androidcoursekfu2/presentation/models/TestModel.kt
@@ -10,13 +10,22 @@ data class TestModel(
 
         fun getBigData(): ArrayList<TestModel> {
             val result = arrayListOf<TestModel>()
-            for (i in 1..10)
-                result.add(
-                    TestModel(
-                        "Title${i}",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            for (i in 1..5) {
+                if (i == 2)
+                    result.add(
+                        TestModel(
+                            "asdfqwerqwefasdvzxcbdfgetqwerggdfbcvbsfhwertqertbxcvnsfghqerteqrgadvzxcbfdhweryqertzxbcvnsryt",
+                            "desc)"
+                        )
                     )
-                )
+                else
+                    result.add(
+                        TestModel(
+                            "Title${i}",
+                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                        )
+                    )
+            }
             return result
         }
 
